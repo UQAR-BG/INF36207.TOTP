2cffe71ad032010032632df05916738efaeffdb8
Idriss7734 <80236304+Idriss7734@users.noreply.github.com>
2023-02-04 15:58:21 -0500

    Update ClientForm.cs
    add comments

--- Client/INF36207.TOTP.Client/ClientForm.cs
+++ Client/INF36207.TOTP.Client/ClientForm.cs
@@ -29,6 +29,11 @@ namespace INF36207.TOTP.Client
             _otpService = new TotpService(_counterService, _hashService, _secretKey, _otpLength);
         }
 
+        /// <summary>
+        /// Set value in the form componenets on load.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ClientForm_Load(object sender, EventArgs e)
         {
             lblCountdown.Text = GetSecondsBeforeNextOtp();
@@ -37,6 +42,11 @@ namespace INF36207.TOTP.Client
             timerCountdown.Start();
         }
 
+        /// <summary>
+        /// Simple function that print the OTP. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void timerCountdown_Tick(object sender, EventArgs e)
         {
             lblCountdown.Text = GetSecondsBeforeNextOtp();
@@ -47,17 +57,31 @@ namespace INF36207.TOTP.Client
             }
         }
 
+        /// <summary>
+        /// This close the timer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
         {
             timerCountdown.Stop();
         }
 
+        /// <summary>
+        /// Print the time avalible before the next OTP. 
+        /// </summary>
+        /// <returns></returns>
         private string GetSecondsBeforeNextOtp()
         {
             long seconds = _counterService.SecondsBeforeNextOtp(_otpLifetime);
             return _counterService.Format(seconds);
         }
 
+        /// <summary>
+        /// This function enable a button to copy the OTP the the clipboard.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnCopyToClipboard_Click(object sender, EventArgs e)
         {
             string otp = _otpService.CurrentOtp;
